4a908cb3d52a5e5fb6d60d8a1b4648d082d1405a
gdbmanu <gras.de.bouillon@free.fr>
2019-07-09 16:18:01 +0200

    updating slides

--- 2019-07-15_CNS.py
+++ 2019-07-15_CNS.py
@@ -303,7 +303,7 @@ url =  'full code @ <a href="https://github.com/SpikeAI/2019-07-15_CNS/">github.
 ####################### SLIDE B 0 ##################################
 
 s.add_slide(content=s.content_figures(
-[os.path.join(figpath_talk, '' )],
+[os.path.join(figpath_talk, 'what_where_brain.png' )],
         title='Active Vision', height=s.meta['height']*.825),
 notes="""
 
