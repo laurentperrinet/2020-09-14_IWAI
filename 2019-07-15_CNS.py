4d613c17aa90706f3819a3c8e1cb52dcc4dfdb7d
gdbmanu <gras.de.bouillon@free.fr>
2019-07-12 14:01:16 +0200

    updating slides

--- 2019-07-15_CNS.py
+++ 2019-07-15_CNS.py
@@ -441,7 +441,7 @@ TODO Manu : générer images correctes avec leur saccades + incorrectes (fake)
 
 s.add_slide(content=s.content_figures(
 [os.path.join(figpath_talk, 'fig_result_robust_contrast_linear_0.7_1.png') ],
-title=title, height=s.meta['height']*.5, transpose=True),
+title=title, height=s.meta['height']*.45, transpose=True),
 notes="""
 TODO Manu : insérer résultats avec différents contrastes
 
