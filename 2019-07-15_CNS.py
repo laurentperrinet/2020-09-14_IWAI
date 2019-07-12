2b471b61d9a66928a58bc55d4309d58a30214f3a
gdbmanu <gras.de.bouillon@free.fr>
2019-07-12 22:46:03 +0200

    updating slides

--- 2019-07-15_CNS.py
+++ 2019-07-15_CNS.py
@@ -510,7 +510,7 @@ TODO Manu : insérer résultats avec différents contrastes
 
 
 s.add_slide(content=s.content_figures(
-[os.path.join(figpath_talk, 'fig_result_robust_contrast_linear_0.7_1.png'),
+[#os.path.join(figpath_talk, 'fig_result_robust_contrast_linear_0.7_1.png'),
 os.path.join(figpath_talk, 'CNS-results-contrast.svg') ],
 title=title + ': role of contrast', height=s.meta['height']*height_ratio, transpose=True),
 notes="""
