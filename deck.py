b2c3e7b6d9e63ef1ed8e05ade43673c02d9d2d04
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-09-10 10:20:34 +0200

    going OO

--- deck.py
+++ deck.py
@@ -3,12 +3,12 @@ __licence__ = 'GPL licence'
 import os
 ################################################################################
 class Slide:
-    def __init__(self, filenames=[], subtitles=[], texts=[], fontsizes=[],
-                 color='white', bg_color='black', duration=1):
-        self.filenames = filenames
+    def __init__(self, contents=[], subtitles=[], fontsizes=[],
+                 type='text', color='white', bg_color='black', duration=1):
+        self.type = type
         self.subtitles = subtitles
         self.duration = duration
-        self.texts = texts
+        self.contents = contents
         self.fontsizes = fontsizes
         self.color = color
         self.bg_color = bg_color
@@ -23,55 +23,49 @@ class Deck:
                  figpath = 'figures',
                  H=500, W=800, border_ratio=3):
 
-        self.W = W
-        self.H = H
+        # self.W = W
+        # self.H = H
+        self.H_fig, self.W_fig = int(H-H/(1.618*border_ratio)), int(W-W/(1.618*border_ratio))
+
         self.videoname = videoname
         self.gifname = videoname + ".gif"
         self.figpath = figpath
         self.fps = fps
 
-        self.H_fig, self.W_fig = int(H-H/(1.618*border_ratio)), int(W-W/(1.618*border_ratio))
 
-        # self.opt_st = dict(font="Open-Sans-SemiBold", size=(W,H), method='caption')size=(W,H), method='caption',
-        self.txt_opts = dict(font="Open-Sans-Regular", align='center', color='white',)
+        self.txt_opts = dict(font="Open-Sans-Regular", align='center', color='white',
+                             size=(W,H), method='caption')
         #self.txt_opts = dict(fontsize=65, bg_color='white', align='center', **opt_st)
-        self.sub_opts = dict(font="Open-Sans-SemiBold", fontsize=28, align='South', color='yellow')
+        self.sub_opts = dict(font="Open-Sans-SemiBold", fontsize=28, align='South', color='yellow', size=(W,H), method='caption')
 
     def compositing(self, slides):
         t = 0
         clips = []
         for slide in slides:
             # contains figures or text
-            if len(slide.filenames)>0:
-                # drawing the list of figures
-                sub_duration = slide.duration / len(slide.filenames)
-                for filename in slide.filenames:
-                    clip = ImageClip(os.path.join(self.figpath, filename))
-                    # time
-                    t += sub_duration
-                    clip = clip.set_start(t).set_duration(sub_duration)
-                    # space
-                    clip = clip.set_pos('center')
-                    clip = clip.resize(height=self.H_fig, width=self.W_fig)
-            else:
-                sub_duration = slide.duration / len(slide.texts)
-                for i_text, text in enumerate(slide.texts):
-                    if len(slide.fontsizes)==0:
-                        fontsize = 35 # default
-                    elif len(slide.fontsizes)==1:
-                        fontsize = slide.fontsizes
+            if len(slide.contents)>0:
+                sub_duration = slide.duration / len(slide.contents)
+                for i_, content in enumerate(slide.contents):
+                    if slide.type == 'text':
+                        if len(slide.fontsizes)==0:
+                            fontsize = 35 # default
+                        elif len(slide.fontsizes)==1:
+                            fontsize = slide.fontsizes
+                        else:
+                            fontsize = slide.fontsizes[i_]
+
+                        clip = TextClip(content, fontsize=fontsize, **self.txt_opts)
                     else:
-                        fontsize = slide.fontsizes[i_text]
-
-                    clip = TextClip(text, fontsize=fontsize, **self.txt_opts)
+                        # drawing the list of figures
+                        clip = ImageClip(os.path.join(self.figpath, content))
                     # time
-                    t += sub_duration
                     clip = clip.set_start(t).set_duration(sub_duration)
                     # space
-                    clip = clip.set_pos('center')
                     clip = clip.resize(height=self.H_fig, width=self.W_fig)
+                    clip = clip.set_pos('center')
 
-                clips.append(clip)
+                    clips.append(clip)
+                    t += sub_duration
 
             if len(slide.subtitles)>0:
                 # overlaying subtitles
@@ -87,7 +81,10 @@ class Deck:
     def compiling(self, clips):
         # Compostiting all clips
         video = CompositeVideoClip(clips)
+        print('Writing', self.videoname + '.mp4')
         video.write_videofile(self.videoname + '.mp4', fps=self.fps)
+        print('Writing', self.gifname)
         video.write_gif(self.gifname, fps=self.fps)
         from pygifsicle import optimize
         optimize(self.gifname)
+        print('Done')
